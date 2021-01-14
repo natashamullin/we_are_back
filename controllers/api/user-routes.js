bc8834b4637532130d0372be7a89e9bbf00bf971
Adam Barron <adambarron@me.com>
2021-01-14 00:13:27 -0800

    added bcrypt functionality and /api/users/login post route along with user verification. Verified that the /login route works properly in Insomnia Core

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -48,6 +48,27 @@ router.post('/', (req, res) => {
         });
 });
 
+// User Login
+router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address foound' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+        res.json({ user: dbUserData, message: 'You are now logged in.' });
+    })
+})
+
 // UPDATE a user
 router.put('/:id', (req, res) => {
     User.update(req.body, {
