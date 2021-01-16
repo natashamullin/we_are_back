96c0c0726cae15525de310c7ad2a70497c2aa7e0
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-01-15 21:26:16 -0800

    causing ruckus

---
+++ controllers/api/review-routes.js
@@ -0,0 +1,101 @@
+const router = require('express').Router();
+const { Review, User } = require('../../models');
+const withAuth = require('../../utils/auth')
+
+router.get('/', (req, res) => {
+    Review.findAll({
+        include: [
+            {
+                model: User
+            }
+        ]
+    })
+        .then(dbReviewData => res.json(dbReviewData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.get('/:id', withAuth, (req, res) => {
+    Review.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User
+            }
+        ]
+    })
+        .then(dbReviewData => {
+            if (!dbReviewData) {
+                res.status(404).json({ message: 'No review found with this id' });
+                return;
+            }
+            res.json(dbReviewData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.post('/', withAuth, (req, res) => {
+    Review.create({
+        title: req.body.title,
+        review_body: req.body.review_body,
+        user_id: req.body.user_id
+    })
+        .then(dbReviewData => res.json(dbReviewData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.put('/:id', withAuth, (req, res) => {
+    Review.update(
+        {
+            title: req.body.title,
+            review_body: req.body.review_body
+        },
+        {
+            where: {
+                id: req.params.id
+            }
+        }
+    )
+        .then(dbReviewData => {
+            if (!dbReviewData) {
+                res.status(404).json({ message: 'No review found with this id' });
+                return;
+            }
+            res.json(dbReviewData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Review.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbReviewData => {
+            if (!dbReviewData) {
+                res.status(404).json({ message: 'No review found with this id' });
+                return;
+            }
+            res.json(dbReviewData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
