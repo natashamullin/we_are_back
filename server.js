2b14d94f2f3dafaa39cdcaacd324ff39f7dc4fe9
Natasha Mullin <69641107+natashamullin@users.noreply.github.com>
2021-01-20 20:14:11 -0800

    changed force false to true

--- server.js
+++ server.js
@@ -32,6 +32,6 @@ app.use(passport.session());
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'));
 });
